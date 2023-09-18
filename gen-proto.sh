2cad3ab308f5400900ae9ad826859ea046f9d50b
从零开始的程序员生活 <793932470@qq.com>
2023-09-18 18:42:26 +0800

    update

--- gen-proto.sh
+++ gen-proto.sh
@@ -2,6 +2,6 @@
 
 PROTO_PREFIX=github.com/kanengo/egoist
 
-protoc --proto_path=. --go_out=. --go_opt=module="$PROTO_PREFIX" --go-grpc_out=. --go-grpc_opt=module="$PROTO_PREFIX" ./api/v1/*.proto
+protoc --proto_path=protos/3rd --proto_path=protos --go_out=. --go_opt=module="$PROTO_PREFIX" --go-grpc_out=. --go-grpc_opt=module="$PROTO_PREFIX" protos/api/v1/*.proto
 
-#protoc --proto_path=. --go_out=. --go_opt=module=github.com/kanengo/egoist --go-grpc_out=. --go-grpc_opt=module=github.com/kanengo/egoist ./api/v1/*.proto
+#protoc --proto_path=protos --go_out=. --go_opt=module="$PROTO_PREFIX" --go-grpc_out=. --go-grpc_opt=module="$PROTO_PREFIX" protos/api/v1/*.proto
